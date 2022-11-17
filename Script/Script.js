b54a01149bd26b0444bee8f6e20a1138aa9cb667
Vexouu <ewennpro@gmail.com>
2022-11-17 23:25:25 +0100

    V1.7

--- Script/Script.js
+++ Script/Script.js
@@ -1,6 +1,7 @@
 let id = null;
 let sIndex = 0;
-        
+let i =0
+
 function showImage(n) {
    sIndex=n;
    displaySlide();
@@ -14,7 +15,7 @@ function myMove() {
 
 
 function displaySlide() {
-    let i;
+    let i=0;
     let slide = document.getElementsByClassName("slide");
 
     if (sIndex >= slide.length) {
@@ -32,4 +33,126 @@ function displaySlide() {
     slide[sIndex].style.display = "block";
     
     sIndex++;
+}
+
+
+function IndexFacebookB(){
+    let image = document.getElementById("facebook")
+    if (i==0){
+        image.src = "img/facebookB.png"
+        i=1;
+    }
+    else{
+        image.src = "img/facebook.png"
+        i=0;
+    }
+}
+
+function IndexDiscordB(){
+    let image = document.getElementById("discord")
+    if (i==0){
+        image.src = "img/discordB.png"
+        i=1;
+    }
+    else{
+        image.src = "img/discord.png"
+        i=0;
+    }
+}
+
+function IndexInstaB(){
+    let image = document.getElementById("insta")
+    if (i==0){
+        image.src = "img/instaB.png"
+        i=1;
+    }
+    else{
+        image.src = "img/insta.png"
+        i=0;
+    }
+}
+
+function IndexTwitterB(){
+    let image = document.getElementById("twitter")
+    if (i==0){
+        image.src = "img/twitterB.png"
+        i=1;
+    }
+    else{
+        image.src = "img/twitter.png"
+        i=0;
+    }
+}
+
+function IndexTwitchB(){
+    let image = document.getElementById("twitch")
+    if (i==0){
+        image.src = "img/twitchB.png"
+        i=1;
+    }
+    else{
+        image.src = "img/twitch.png"
+        i=0;
+    }
+}
+
+
+function FacebookB(){
+    let image = document.getElementById("facebook")
+    if (i==0){
+        image.src = "../img/facebookB.png"
+        i=1;
+    }
+    else{
+        image.src = "../img/facebook.png"
+        i=0;
+    }
+}
+
+function DiscordB(){
+    let image = document.getElementById("discord")
+    if (i==0){
+        image.src = "../img/discordB.png"
+        i=1;
+    }
+    else{
+        image.src = "../img/discord.png"
+        i=0;
+    }
+}
+
+function InstaB(){
+    let image = document.getElementById("insta")
+    if (i==0){
+        image.src = "../img/instaB.png"
+        i=1;
+    }
+    else{
+        image.src = "../img/insta.png"
+        i=0;
+    }
+}
+
+function TwitterB(){
+    let image = document.getElementById("twitter")
+    if (i==0){
+        image.src = "../img/twitterB.png"
+        i=1;
+    }
+    else{
+        image.src = "../img/twitter.png"
+        i=0;
+    }
+}
+
+function TwitchB(){
+    let image = document.getElementById("twitch")
+    if (i==0){
+        image.src = "../img/twitchB.png"
+        i=1;
+    }
+    else{
+        image.src = "../img/twitch.png"
+        i=0;
+    }
 }
